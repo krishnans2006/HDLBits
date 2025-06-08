471def7d066fb9dbecb4e89c703b0f59ffaab1ef
Krishnan Shankar <krishnans2006@gmail.com>
2025-06-07 23:05:55 -0400

    Finish module_pos

---
+++ 021module_pos.v
@@ -0,0 +1,14 @@
+module mod_a ( output, output, input, input, input, input );
+    // Module body
+endmodule
+
+module top_module ( 
+    input a, 
+    input b, 
+    input c,
+    input d,
+    output out1,
+    output out2
+);
+    mod_a mod ( out1, out2, a, b, c, d );
+endmodule
