171fe9c639c0ff256177fc105e8a49dccb400d93
Krishnan Shankar <krishnans2006@gmail.com>
2025-06-17 21:25:29 -0400

    Finish mt2015_q4

---
+++ 02Verilog_Language/055mt2015_q4.v
@@ -0,0 +1,12 @@
+module top_module (
+    input x,
+    input y,
+    output z
+);
+    wire a, b;
+
+    assign a = (x ^ y) & x;
+    assign b = (x == y);
+
+    assign z = (a | b) ^ (a & b);
+endmodule
